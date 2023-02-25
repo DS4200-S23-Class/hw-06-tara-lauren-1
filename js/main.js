6d623f1c84853e73bd8c0acc3f1daa7900643fd6
tarajoshii <119441296+tarajoshii@users.noreply.github.com>
2023-02-25 15:27:22 -0500

    bars not showing :(

--- js/main.js
+++ js/main.js
@@ -8,53 +8,56 @@ const VIS_WIDTH = FRAME_WIDTH - MARGINS.left - MARGINS.right;
 
 
 
-function build_interactive_bar() {
-    d3.csv("data/iris.csv").then((data3) => {
-        
-        const FRAME3 = d3.select("#barchart")
-                          .append("svg")
-                          .attr("height", FRAME_HEIGHT)
-                          .attr("width", FRAME_WIDTH)
-                          .attr("class", "frame"); 
 
+const FRAME3 = d3.select("#vis3")
+                    .append("svg")
+                    .attr("height", FRAME_HEIGHT)
+                    .attr("width", FRAME_WIDTH)
+                    .attr("class", "frame"); 
+
+function build_interactive_bar() {
+
+    d3.csv("data/iris.csv").then((data) => {
 
         const X_SCALE3 = d3.scaleBand()
                            .range([0, VIS_WIDTH])
                            .domain(["setosa", "versicolor", "virginica"]);
 
         const Y_SCALE3 = d3.scaleBand()
-                           .range([0, VIS_WIDTH])
-                           .domain([80,0]);
+                           .range([0, VIS_HEIGHT])
+                           .domain([100,0]);
 
 
         // Adding X Axis 
 
         FRAME3.append("g") 
-            .attr("transform", "translate(" + MARGINS.left + "," + 
+              .attr("transform", "translate(" + MARGINS.left + "," + 
                 (VIS_HEIGHT + MARGINS.top) + ")") 
-            .call(d3.axisBottom(X_SCALE3).ticks(4)) 
-            .attr("font-size", '20px'); 
+              .call(d3.axisBottom(X_SCALE3)) 
+              .attr("font-size", '20px'); 
 
         // Adding Y Axis
 
-        FRAME2.append("g") 
+        FRAME3.append("g") 
             .attr("transform", "translate(" + MARGINS.left +
               "," + (MARGINS.bottom) + ")") 
-            .call(d3.axisLeft(Y_SCALE3).ticks(4)) 
+            .call(d3.axisLeft(Y_SCALE3)) 
             .attr("font-size", '20px');
 
 
         // Adding bars
 
-        FRAME2.selectAll(".bar")
+        FRAME3.selectAll(".bar")
                 .data(data)
                 .enter()
                 .append("rect")
-                .attr("x", function(d) { return X_SCALE3(d.species) + MARGINS.left;})
-                .attr("width", X_SCALE3.bandwidth())
-                .attr("y", 50 + MARGINS.bottom);
-                .attr("height", function(d) { return VIS_HEIGHT - 50;})
-                .attr("class", "bar");})}
+                    .attr("x", function(d) { return X_SCALE3(d.Species) + MARGINS.left;})
+                    .attr("width", X_SCALE3.bandwidth())
+                    .attr("y", 50 + MARGINS.bottom);
+                    .attr("height", VIS_HEIGHT - 50);
+                    .attr("class", "bar");})}
+
 
 
 build_interactive_bar()
+
